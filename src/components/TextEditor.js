f81193e20fe9cf73451de01428033bced5a77527
User12 <dgpradeepal@gmail.com>
2019-05-06 15:26:33 +0530

    Adds 4 mark types

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -33,11 +33,51 @@ const initialValue = Value.fromJSON({
   
 
 
-export  class TextEditor extends Component{
+export  class TextEditor extends React.Component{
 
     state = {
         value: initialValue,
     }
+
+    /**
+   * Check if the current selection has a mark with `type` in it.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  hasMark = type => {
+    const { value } = this.state
+    return value.activeMarks.some(mark => mark.type === type)
+  }
+
+  /**
+   * Check if the any of the currently selected blocks are of `type`.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  hasBlock = type => {
+    const { value } = this.state
+    return value.blocks.some(node => node.type === type)
+  }
+
+  /**
+   * Store a reference to the `editor`.
+   *
+   * @param {Editor} editor
+   */
+
+  ref = editor => {
+    this.editor = editor
+  }
+
+
+
+
+
+
 
     onChange = ({value}) =>{
         this.setState({value})
@@ -56,6 +96,21 @@ export  class TextEditor extends Component{
           editor.toggleMark('bold')
           break
         }
+        case 'i': {
+          event.preventDefault()
+          editor.toggleMark('italic')
+          break
+        }
+        case 'u': {
+          event.preventDefault()
+          editor.toggleMark('underlined')
+          break
+        }
+        case '`': {
+          event.preventDefault()
+          editor.toggleMark('code')
+          break
+        }
         default: {
             return next()
           }
@@ -63,32 +118,45 @@ export  class TextEditor extends Component{
     }
   }
 
+  
+
+  /**
+   * Render a Slate mark.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
   renderMark = (props, editor, next) => {
-    switch (props.mark.type) {
+    const { children, mark, attributes } = props
+
+    switch (mark.type) {
       case 'bold':
-        return <BoldMark {...props} />
+        return <strong {...attributes}>{children}</strong>
+      case 'code':
+        return <code {...attributes}>{children}</code>
+      case 'italic':
+        return <em {...attributes}>{children}</em>
+      case 'underlined':
+        return <u {...attributes}>{children}</u>
       default:
         return next()
     }
   }
 
 
-    render() {
+
+    render(editor) {
         return (
           <div>
             <Toolbar>
-              {this.renderMarkButton('bold', 'format_bold')}
-              {this.renderMarkButton('italic', 'format_italic')}
-              {this.renderMarkButton('underlined', 'format_underlined')}
-              {this.renderMarkButton('code', 'code')}
-              {this.renderBlockButton('heading-one', 'looks_one')}
-              {this.renderBlockButton('heading-two', 'looks_two')}
-              {this.renderBlockButton('block-quote', 'format_quote')}
-              {this.renderBlockButton('numbered-list', 'format_list_numbered')}
-              {this.renderBlockButton('bulleted-list', 'format_list_bulleted')}
+              {this.renderMarkButton('bold', editor,'format_bold')}
+              {this.renderMarkButton('italic',editor, 'format_italic')}
+              {this.renderMarkButton('underlined', editor, 'format_underlined')}
+              {this.renderMarkButton('code', editor ,'code')}
             </Toolbar>
             <Editor
-
+             ref={this.ref}
              value={this.state.value}
              onChange={this.onChange} 
              onKeyDown={this.onKeyDown}
@@ -107,13 +175,13 @@ export  class TextEditor extends Component{
    * @return {Element}
    */
 
-  renderMarkButton = (type, icon) => {
+  renderMarkButton = (type, editor,icon) => {
     const isActive = this.hasMark(type)
 
     return (
       <Button
         active={isActive}
-        onMouseDown={event => this.onClickMark(event, type)}
+        onMouseDown={event => this.onClickMark(event, editor, type)}
       >
         <Icon>{icon}</Icon>
       </Button>
@@ -132,39 +200,17 @@ export  class TextEditor extends Component{
     return value.activeMarks.some(mark => mark.type === type)
 }
 
-renderBlockButton = (type, icon) => {
-  let isActive = this.hasBlock(type)
-
-  if (['numbered-list', 'bulleted-list'].includes(type)) {
-    const { value: { document, blocks } } = this.state
-
-    if (blocks.size > 0) {
-      const parent = document.getParent(blocks.first().key)
-      isActive = this.hasBlock('list-item') && parent && parent.type === type
-    }
-  }
-
-  return (
-    <Button
-      active={isActive}
-      onMouseDown={event => this.onClickBlock(event, type)}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
 
 /**
-   * Check if the any of the currently selected blocks are of `type`.
+   * When a mark button is clicked, toggle the current mark.
    *
+   * @param {Event} event
    * @param {String} type
-   * @return {Boolean}
    */
 
-  hasBlock = type => {
-    const { value } = this.state
-    return value.blocks.some(node => node.type === type)
+  onClickMark = (event, editor, type) => {
+    event.preventDefault()
+    this.editor.toggleMark(type)
 }
-
 
 }
